da4ac95454479d37ee95c0b0da8c025b78a80e28
Robin Cunningham <68775372+robincunningham2@users.noreply.github.com>
2021-08-24 21:19:46 +0200

    Create a bump.js script to bump the version easily

---
+++ scripts/bump.js
@@ -0,0 +1,48 @@
+#!/usr/local/bin/node
+/**
+ * Bump the version on package.json and package-lock.json
+ * Yes, I realise how shitty this script is
+ * No, I don't plan on doing anything about it
+ * 
+ * Usage:
+ *   npm run bump [x.x.x]
+ *   npm run bump [major|minor|patch]
+ */
+
+const fs = require('fs');
+
+let package = require('../package.json');
+let package_lock = require('../package-lock.json');
+
+function bump(bumping) {
+    let version = package.version.split('.');
+    let toBump = 2;
+
+    if (bumping == 'major') toBump = 0;
+    else if (bumping == 'minor') toBump = 1;
+    else if (bumping == 'patch') toBump = 2;
+    else if (bumping.indexOf('.')) {
+        version = bumping.split('.');
+        toBump = -1;
+    }
+
+    version[toBump]++;
+    version = version.join('.');
+
+    package.version = version;
+    package_lock.version = version;
+
+    return Promise.all([
+        new Promise(resolve => fs.writeFile('package.json', JSON.stringify(package, null, 2) + '\n', resolve)),
+        new Promise(resolve => fs.writeFile('package-lock.json', JSON.stringify(package_lock, null, 2) + '\n', resolve))
+    ]);
+}
+
+function cli() {
+    let bumping = process.argv[1] || '';
+    if ((process.argv0 || '').indexOf('node') != -1) bumping = process.argv[2] || '';
+
+    bump(bumping).then(() => console.log(`Bumped the version to ${package.version}`));
+}
+
+cli();
