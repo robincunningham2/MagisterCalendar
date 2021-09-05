c867ae429b7eabf03771a707b25c648c4f78b71f
Robin Cunningham <robinmichaelcunningham@gmail.com>
2021-09-05 19:44:30 +0200

    Refresh the token automatically

--- src/calendar.js
+++ src/calendar.js
@@ -1,11 +1,34 @@
 const Config = require('../config/index');
 const { google } = require('googleapis');
+const got = require('got').default;
 
 class GoogleCalendar {
     constructor() {
         this.scopes = Config.googleApis.scopes;
         this.client = null;
         this.calendar = null;
+    }
+
+    async _generateAccessToken(token) {
+        console.log({
+            client_id: Config.credentials.installed.client_id,
+            client_secret: Config.credentials.installed.client_secret,
+            refresh_token: token.refresh_token,
+            grant_type: 'refresh_token'
+        });
+        let response = await got.post('https://www.googleapis.com/oauth2/v4/token', {
+            json: {
+                client_id: Config.credentials.installed.client_id,
+                client_secret: Config.credentials.installed.client_secret,
+                refresh_token: token.refresh_token,
+                grant_type: 'refresh_token'
+            }
+        }).json();
+
+        token.access_token = response.access_token;
+        token.expiry_date = Number(new Date) + response.expires_in * 1000;
+
+        return token;
     }
 
     _generateEventOptions(appointment) {
@@ -40,25 +63,25 @@ class GoogleCalendar {
         if (!token) {
             const url = this.client.generateAuthUrl({
                 access_type: 'offline',
-                scope: this.scopes,
+                scope: this.scopes
             });
 
             await new Promise((resolve, reject) => {
                 callback(url).then(code => {
-                    this.client.getToken(code, (err, tok) => {
+                    this.client.getToken(code, (err, t) => {
                         if (err) return reject(err);
-                        this.client.setCredentials(tok);
-
-                        token = tok;
-                        resolve(this);
+                        token = t;
+                        resolve();
                     });
                 });
             });
-        } else {
-            this.client.setCredentials(token);
+        } else if (new Date > token.expiry_date - 1000) {
+            token = await this._generateAccessToken(token);
         }
 
+        this.client.setCredentials(token);
         this.calendar = google.calendar({ version: 'v3', auth: this.client });
+
         return { authorized: true, token };
     };
 
