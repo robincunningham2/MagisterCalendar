94ae27b1d3ad631c86438fff4ae44a1f826650e9
Robin Cunningham <robinmichaelcunningham@gmail.com>
2021-09-05 15:31:22 +0200

    Update configuration

--- config/index.js
+++ config/index.js
@@ -1,13 +1,13 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-module.exports = {
-    googleapis: {
-        defaultCalendar: process.env.CALENDAR_ID || 'primary',
-        timeZone: process.env.TZ || 'Europe/Amsterdam',
-        scopes: (
-            process.env.SCOPES ||
-            'https://www.googleapis.com/auth/calendar,https://www.googleapis.com/auth/calendar.events'
-        ).split(',')
-    }
+const config = require('./config');
+const credentials = require('./credentials.json');
+
+if (process.env.MAGISTER_CALENDAR) module.exports = JSON.parse(process.env.MAGISTER_CALENDAR);
+else {
+    module.exports = {
+        ...config,
+        credentials
+    };
 }
