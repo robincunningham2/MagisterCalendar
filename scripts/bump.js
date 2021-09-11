d5cd086ed28eb05892c9c3b5d1b6eba37b8ba11c
Robin Cunningham <robinmichaelcunningham@gmail.com>
2021-09-11 20:29:08 +0200

    Fix the bump script

--- scripts/bump.js
+++ scripts/bump.js
@@ -1,12 +1,15 @@
 #!/usr/local/bin/node
 /**
- * Bump the version on package.json and package-lock.json
- * Yes, I realise how shitty this script is
- * No, I don't plan on doing anything about it
+ * Easily change the version in the package.json and package-lock.json files
  *
  * Usage:
- *   npm run bump [x.x.x]
- *   npm run bump [major|minor|patch]
+ *   npm run bump [n.n.n|keyword]?
+ * 
+ * Examples:
+ *   npm run bump 1.0.0     Change the version to 1.0.0
+ *   npm run bump major     Increase the major value of the current version, and set the minor and patch values to 0
+ *   npm run bump patch     Increase the patch value of the current version
+ *   npm run bump           Same as `npm run bump patch`
  */
 
 const fs = require('fs');
@@ -14,36 +17,25 @@ const fs = require('fs');
 const package = require('../package.json');
 const packageLock = require('../package-lock.json');
 
-function bump(bumping) {
-    let version = package.version.split('.');
-    let toBump = 2;
+function cli() {
+    const oldVersion = package.version;
+    let version = oldVersion.split('.');
 
-    if (bumping == 'major') toBump = 0;
-    else if (bumping == 'minor') toBump = 1;
-    else if (bumping == 'patch') toBump = 2;
-    else if (bumping.indexOf('.')) {
-        version = bumping.split('.');
-        toBump = -1;
-    }
+    let arg = process.argv[1] || '';
+    if ((process.argv0 || '').indexOf('node') != -1) arg = process.argv[2] || '';
 
-    version[toBump]++;
-    version = version.join('.');
+    if (arg.indexOf('.') != -1) version = arg;
+    else if (arg == 'major') version = `${Number(version[0]) + 1}.0.0`;
+    else if (arg == 'minor') version = `${version[0]}.${Number(version[1]) + 1}.0`;
+    else version = `${version.slice(0, 2).join('.')}.${Number(version[2]) + 1}`;
 
     package.version = version;
     packageLock.version = version;
 
-    return Promise.all([
-        new Promise((resolve) => fs.writeFile('package.json', JSON.stringify(package, null, 2) + '\n', resolve)),
-        new Promise((resolve) => fs.writeFile('package-lock.json',
-            JSON.stringify(packageLock, null, 2) + '\n', resolve)),
-    ]);
-}
+    fs.writeFileSync('package.json', JSON.stringify(package, null, 2) + '\n');
+    fs.writeFileSync('package-lock.json', JSON.stringify(packageLock, null, 2) + '\n');
 
-function cli() {
-    let bumping = process.argv[1] || '';
-    if ((process.argv0 || '').indexOf('node') != -1) bumping = process.argv[2] || '';
-
-    bump(bumping).then(() => console.log(`Bumped the version to ${package.version}`));
+    console.log(`Bumped the version to ${version} from ${oldVersion}`);
 }
 
 cli();
