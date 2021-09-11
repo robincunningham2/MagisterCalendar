dd13172e28e39a22274c47b7ab67ff4e5d856c9c
Robin Cunningham <robinmichaelcunningham@gmail.com>
2021-09-11 12:18:17 +0200

    Update config

--- config/index.js
+++ config/index.js
@@ -1,11 +1,11 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-const config = require('./config');
-const credentials = require('./credentials.json');
-
 if (process.env.MAGISTER_CALENDAR) module.exports = JSON.parse(process.env.MAGISTER_CALENDAR);
 else {
+    const config = require('./config');
+    const credentials = require('./credentials.json');
+
     module.exports = {
         ...config,
         credentials,
