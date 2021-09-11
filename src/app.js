3cf13800fc0506143357df9cb002badd841cced5
Robin Cunningham <robinmichaelcunningham@gmail.com>
2021-09-11 11:50:34 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,42 +1,47 @@
 const Config = require('../config/index');
 
-const readline = require('readline');
 const fs = require('fs');
+const path = require('path');
 
 const { Magister } = require('./magister');
-const { GoogleCalendar } = require('./calendar');
+const google = require('./googleServices');
 
-const calendar = new GoogleCalendar(Config);
-const client = new Magister(Config.magister);
+let token = null;
+if (fs.existsSync(path.join(process.cwd(), '.tmp/token.json'))) token = require('../.tmp/token.json');
 
-function callback(url) {
-    console.log(`Authenticate via this URl: ${url}`);
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
-
-    return new Promise((resolve) => {
-        rl.question('Enter the code from the page: ', (code) => {
-            rl.close();
-            resolve(code);
-        });
-    });
-}
-
-let token;
-if (fs.existsSync('.tmp/token.json')) token = require('../.tmp/token.json');
+const calendar = new google.GoogleCalendar(Config, google.defaults.DESKTOP_CALLBACK, token);
+const magister = new Magister(Config.magister);
 
 Promise.all([
-    calendar.authorize(callback, token),
-    client.login(),
+    calendar.authorize(),
+    magister.login(),
 ]).then(async (result) => {
-    token = result[0].token;
-    fs.writeFileSync('.tmp/token.json', JSON.stringify(token));
+    token = result[0];
+    fs.writeFileSync(path.join(process.cwd(), '.tmp/token.json'), JSON.stringify(token));
 
-    const appointments = (await client.get(`/personen/${client.me.Id}/afspraken`)).Items;
+    const items = await calendar.listEvents(
+        new Date(Number(new Date) - 86_400_000).toISOString(),
+        new Date(Number(new Date) + 604_800_000).toISOString()
+    );
 
-    for (let i = 0; i < appointments.length; i++) {
-        await calendar.createEvent(appointments[i]);
+    let existingAppointments = {};
+    items.forEach(item => {
+        const id = ((item.description) || '').slice(
+            ((item.description) || '').indexOf('<!--magisterId:') + 15,
+            ((item.description) || '').indexOf('-->'),
+        );
+
+        if (id) existingAppointments[id] = item;
+    });
+
+    const res = await magister.get(`/personen/${magister.me.Id}/afspraken`);
+    for (let i = 0; i < res.Items.length; i++) {
+        const item = res.Items[i];
+        if (!existingAppointments[String(item.Id)]) await calendar.createEvent(item);
+    }
+
+    for (const id in existingAppointments) {
+        const appointment = await magister.get(`/personen/${magister.me.Id}/afspraken/${id}`);
+        await calendar.updateEvent(appointment, existingAppointments[id]);
     }
 });
