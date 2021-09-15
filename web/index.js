92a6939bb16dc732b37067952d85822271d41e03
Robin Cunningham <robinmichaelcunningham@gmail.com>
2021-09-15 19:38:19 +0200

    Update index.js

--- web/index.js
+++ web/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const cookieSession = require('cookie-session');
+
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'web/views');
+
+app.use(cookieSession({ secret: require('crypto').randomBytes(16).toString('base64') }));
+
+app.use('/static/assets', express.static('web/assets'));
+app.use('/static/css', express.static('web/css'));
+app.use('/static/js', express.static('web/js'));
+
 app.get('/', (_, res) => {
-    res.send('Hello world!');
+    res.end('Index page');
 });
 
 app.use('/api/v1', require('./api/v1'));
@@ -10,8 +21,14 @@ app.use('/api/v1', require('./api/v1'));
 app.use('/api', (_, res) => {
     res.status(404).json({
         status: 404,
-        message: 'The requested page was not found.',
+        message: 'The requested URL was not found.',
     });
+});
+
+app.use('/', (req, res, next) => {
+    if (req.url.startsWith('/static')) return next();
+    // res.status(404).render('errors/404');
+    res.end('404 not found');
 });
 
 app.listen(3000, () => {
