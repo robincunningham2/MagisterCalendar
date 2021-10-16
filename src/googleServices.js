c64c8ce7895ab493f9fa87555b2594b18031f6bd
Robin Cunningham <robinmichaelcunningham@gmail.com>
2021-10-16 14:14:43 +0200

    Update googleServices.js

--- src/googleServices.js
+++ src/googleServices.js
@@ -126,7 +126,7 @@ class GoogleCalendar {
         }
 
         const options = this._generateEventOptions(appointment);
-        options.resource.colorId = Math.max(1, Math.min(11, Number(eventData.colorId) || 8));
+        options.resource.colorId = eventData.colorId;
         options.resource.reminders = eventData.reminders || { useDefault: false };
 
         await this._calendar.events.update({
